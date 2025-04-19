8097e051caf61c299158650e9f76903adee4bf9d
Arnav Agarwal <agarwalarnav2003@gmail.com>
2025-04-19 23:25:40 +0530

    redis lib setup

---
+++ src/lib/redis.js
@@ -0,0 +1,6 @@
+import { Redis } from '@upstash/redis';
+
+export const redis = new Redis({
+  url: 'https://discrete-iguana-16740.upstash.io',
+  token: 'AUFkAAIjcDFkNWQ4NTY5ZmZkOTY0YTg5YWFjOTNiZThlM2NjYWM2OXAxMA',
+});
